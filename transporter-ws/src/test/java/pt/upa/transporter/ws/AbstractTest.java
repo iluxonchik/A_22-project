1d022a6b626d7d1b966c9af82da431c4f6f171c9
Gisson <jorge.heleno@tecnico.ulisboa.pt>
2016-04-10 02:25:39 +0100

    feat: Added tests
    Added 1 IT test ( has nothing inside it :/ )
    Added 2 Unitary tests ( 1 abstract and the other does nothing )
    
    Refers to #10 and #9

---
+++ transporter-ws/src/test/java/pt/upa/transporter/ws/AbstractTest.java
@@ -0,0 +1,34 @@
+package pt.upa.transporter.ws;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public abstract class AbstractTest {
+
+    // one-time initialization and clean-up
+
+    @BeforeClass
+    public static void oneTimeSetUp() {
+
+    }
+
+    @AfterClass
+    public static void oneTimeTearDown() {
+
+    }
+
+    // initialization and clean-up for each test
+    @Before
+    public void populate() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+
+    // tests
+
+    @Test
+    public abstract void test();
+}
---
+++ transporter-ws/src/test/java/pt/upa/transporter/ws/TransporterPortTest.java
@@ -0,0 +1,36 @@
+package pt.upa.transporter.ws;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import mockit.*;
+
+/* Unitary tests */
+
+public class TransporterPortTest extends AbstractTest{
+
+    // initialization and clean-up for each test
+    @Before
+    public void populate() {
+	
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+
+    // tests
+	@Override
+	@Test
+	public void test(){
+	/* FIXME */
+	}
+
+	
+    @Test
+    public void test(@Mocked final TransporterPort tp){
+		/* FIXME */	
+	}
+
+	
+}
