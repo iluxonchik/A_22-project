1c4ee1fc2e1602a93db87e5ad8900afdc79ffad0
ILUXONCHIK ILUXA <iluxon4ik@hotmail.com>
2016-04-11 08:29:40 +0100

    chore: update poms to include shared project

--- broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
+++ broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
@@ -4,7 +4,6 @@ public class BrokerApplication {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(BrokerApplication.class.getSimpleName() + " starting...");
-
 	}
 
 }
