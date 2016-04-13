890381cb6b584ccf9c0e7383c68c78e71834cd9e
ILUXONCHIK ILUXA <iluxon4ik@hotmail.com>
2016-04-13 22:45:07 +0100

    feat: implement BrokerPortType

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,7 +1,32 @@
 package pt.upa.broker.ws;
 
-public class BrokerPort {
 
-	// TODO
+import java.util.List;
+
+public class BrokerPort implements pt.upa.broker.ws.BrokerPortType {
+    @Override
+    public String ping(String name) {
+        return null;
+    }
+
+    @Override
+    public String requestTransport(String origin, String destination, int price) throws pt.upa.broker.ws.InvalidPriceFault_Exception, pt.upa.broker.ws.UnavailableTransportFault_Exception, pt.upa.broker.ws.UnavailableTransportPriceFault_Exception, pt.upa.broker.ws.UnknownLocationFault_Exception {
+        return null;
+    }
+
+    @Override
+    public pt.upa.broker.ws.TransportView viewTransport(String id) throws pt.upa.broker.ws.UnknownTransportFault_Exception {
+        return null;
+    }
+
+    @Override
+    public List<pt.upa.broker.ws.TransportView> listTransports() {
+        return null;
+    }
+
+    @Override
+    public void clearTransports() {
+
+    }
 
 }
