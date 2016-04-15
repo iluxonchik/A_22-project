4302b9febe6708282620c989b680c173f1f86ed2
ILUXONCHIK ILUXA <iluxon4ik@hotmail.com>
2016-04-15 23:22:38 +0100

    test: add more tests to Broker

--- broker-ws/src/test/java/pt/upa/broker/ws/BrokerPortTest.java
+++ broker-ws/src/test/java/pt/upa/broker/ws/BrokerPortTest.java
@@ -6,6 +6,9 @@ import mockit.MockUp;
 import org.junit.*;
 import pt.upa.broker.domain.Broker;
 import pt.upa.broker.domain.BrokerTransportView;
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
 
 /**
  * Created by iluxonchik on 15-04-2016.
@@ -59,6 +62,55 @@ public class BrokerPortTest {
     @Test(expected = UnknownLocationFault_Exception.class)
     public void unknownLocationDestinationFail() throws UnavailableTransportPriceFault_Exception, UnavailableTransportFault_Exception, UnknownLocationFault_Exception, InvalidPriceFault_Exception {
         brokerPort.requestTransport("Evora", "Harlem", 99);
+    }
+
+    @Test
+    public void requestTransportSuccess() throws UnavailableTransportPriceFault_Exception, UnavailableTransportFault_Exception, UnknownLocationFault_Exception, InvalidPriceFault_Exception {
+        new MockUp<Broker>() {
+            @SuppressWarnings("unused")
+            @Mock
+            public BrokerTransportView getCheapestTransporter(String origin, String destination, int maxPrice) {
+                return new BrokerTransportView("Lisboa", "Porto", 50, "BROKER_1");
+            }
+        };
+        assertEquals(brokerPort.requestTransport("Lisboa", "Porto", 60), "BROKER_1");
+    }
+
+    @Test(expected = UnknownTransportFault_Exception.class)
+    public void viewTransportFail() throws UnknownTransportFault_Exception {
+        new MockUp<Broker>() {
+            @SuppressWarnings("unused")
+            @Mock
+            public TransportView getJobById(String id) throws UnknownTransportFault_Exception {
+                UnknownTransportFault faultInfo = new UnknownTransportFault();
+                faultInfo.setId(id);
+                throw new UnknownTransportFault_Exception("Job not found", faultInfo);
+            }
+        };
+        brokerPort.viewTransport("BROKER_123");
+    }
+
+    @Test
+    public void viewTransportSuccess() throws UnavailableTransportPriceFault_Exception, UnavailableTransportFault_Exception, UnknownLocationFault_Exception, InvalidPriceFault_Exception, UnknownTransportFault_Exception {
+        HashMap<String, BrokerTransportView> jobs = new HashMap<>();
+        new MockUp<Broker>() {
+
+            @SuppressWarnings("unused")
+            @Mock
+            public BrokerTransportView getCheapestTransporter(String origin, String destination, int maxPrice) {
+                jobs.put("BROKER_1", new BrokerTransportView(origin, destination, maxPrice, "BROKER_1"));
+                return jobs.get("BROKER_1");
+            }
+
+            @SuppressWarnings("unused")
+            @Mock
+            public TransportView getJobById(String id) throws UnknownTransportFault_Exception {
+                return jobs.get("BROKER_1");
+            }
+        };
+
+        String jobId = brokerPort.requestTransport("Lisboa", "Porto", 90);
+        assertEquals(jobs.get(jobId), brokerPort.viewTransport(jobId));
     }
 
     // tests
